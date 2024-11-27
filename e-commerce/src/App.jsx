b577d7edd6018b2fe1792826ee47350158c34278
Saurabh19 <saurabhsutar0620@gmail.com>
2024-11-28 00:52:52 +0530

    update App.jsx file for add router functionality and routes all components

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import ProductFilterSidebar from "./components/ProductFilterSidebar";
+import ShoppingCartPreview from "./components/ShoppingCartPreview";
+import Wishlist from "./components/Wishlist";
+import OrderConfirmationModal from "./components/OrderConfirmationModal";
+import CheckoutForm from "./components/CheckoutForm";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ProductCard from "./components/ProductCard";
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Navbar cartCount={cart.length} wishlistCount={wishlist.length} />
 
-export default App
+      <div className="flex-1 p-6">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="flex">
+                <ProductFilterSidebar />
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 px-32">
+                  <ProductCard />
+                </div>
+              </div>
+            }
+          />
+          <Route path="/cart" element={<ShoppingCartPreview />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/checkout" element={<CheckoutForm />} />
+          <Route
+            path="/order-confirmation"
+            element={<OrderConfirmationModal />}
+          />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
