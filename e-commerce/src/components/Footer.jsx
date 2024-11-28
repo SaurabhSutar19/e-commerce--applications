e033133935d44ad0eeb0edb04cc46e76d7610aff
Saurabh19 <saurabhsutar0620@gmail.com>
2024-11-28 08:38:17 +0530

    update for add design andfunctionality

--- e-commerce/src/components/Footer.jsx
+++ e-commerce/src/components/Footer.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 Store. All rights reserved.</p>
+        <div className="mt-4">
+          <a href="#" className="mx-3 text-gray-400 hover:text-white">
+            Privacy Policy
+          </a>
+          <a href="#" className="mx-3 text-gray-400 hover:text-white">
+            Terms of Service
+          </a>
+          <a href="#" className="mx-3 text-gray-400 hover:text-white">
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
