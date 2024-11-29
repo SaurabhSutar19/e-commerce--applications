def139b7c4324c5c3a0e65e2e2e551e787917a6d
Saurabh19 <saurabhsutar0620@gmail.com>
2024-11-29 11:32:40 +0530

    update for HashRouter

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import ProductFilterSidebar from "./components/ProductFilterSidebar";
 import ShoppingCartPreview from "./components/ShoppingCartPreview";
