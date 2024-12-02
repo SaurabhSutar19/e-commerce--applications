515830c44e77415359b42b5268a8dc682db44e64
Saurabh19 <saurabhsutar0620@gmail.com>
2024-12-02 10:03:55 +0530

    add responsive code for mobile screen

--- e-commerce/src/components/CheckoutForm.jsx
+++ e-commerce/src/components/CheckoutForm.jsx
@@ -82,38 +82,42 @@ const CheckoutForm = ({ cart }) => {
   };
 
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md hover:shadow-lg max-w-lg sm:max-w-2xl mx-auto">
-      <form onSubmit={handleSubmit} className="space-y-6">
-        <h3 className="text-xl font-semibold text-center text-gray-600 hover:text-indigo-600">
+    <div className="p-4 bg-white rounded-lg shadow-md hover:shadow-lg  max-w-lg mx-auto">
+      <form onSubmit={handleSubmit}>
+        <h3 className="text-xl font-semibold mb-4 text-center text-gray-600 hover:text-indigo-600">
           Checkout
         </h3>
 
         {/* Name Field */}
-        <div>
-          <label className="block text-gray-600 font-bold mb-1">Name:</label>
+        <div className="mb-4">
+          <label className="block font-bold text-gray-600 mb-1 ml-1">
+            Name :
+          </label>
           <input
             type="text"
             name="name"
             value={formData.name}
             onChange={handleInputChange}
-            className={`border-2 rounded-lg p-2 w-full text-sm ${
-              errors.name ? "border-red-500" : "border-gray-300"
-            } focus:outline-none focus:border-indigo-500`}
+            className={`border-2 rounded-lg p-2 w-full border-gray-600 hover:border-indigo-600   ${
+              errors.name ? "border-red-500" : ""
+            }`}
           />
           {errors.name && <p className="text-red-500 text-sm">{errors.name}</p>}
         </div>
 
         {/* Address Field */}
-        <div>
-          <label className="block text-gray-600 font-bold mb-1">Address:</label>
+        <div className="mb-4">
+          <label className="block font-bold text-gray-600 mb-2 ml-1">
+            Address :
+          </label>
           <input
             type="text"
             name="address"
             value={formData.address}
             onChange={handleInputChange}
-            className={`border-2 rounded-lg p-2 w-full text-sm ${
-              errors.address ? "border-red-500" : "border-gray-300"
-            } focus:outline-none focus:border-indigo-500`}
+            className={`border-2 rounded-lg p-2 w-full border-gray-600 hover:border-indigo-600  ${
+              errors.address ? "border-red-500" : ""
+            }`}
           />
           {errors.address && (
             <p className="text-red-500 text-sm">{errors.address}</p>
@@ -121,11 +125,11 @@ const CheckoutForm = ({ cart }) => {
         </div>
 
         {/* Payment Details */}
-        <div>
-          <h4 className="text-lg font-semibold text-center sm:text-left text-gray-700 mb-2">
-            Payment Details
-          </h4>
-          <label className="block text-gray-600 font-bold mb-1">
+        <h4 className="text-lg text-center font-semibold mb-2 text-gray-600 hover:text-indigo-600">
+          Payment Details
+        </h4>
+        <div className="mb-4">
+          <label className="block font-bold text-gray-600 mb-2 ml-1">
             Credit Card Number
           </label>
           <input
@@ -133,20 +137,18 @@ const CheckoutForm = ({ cart }) => {
             name="creditCard"
             value={formData.creditCard}
             onChange={handleInputChange}
-            className={`border-2 rounded-lg p-2 w-full text-sm ${
-              errors.creditCard ? "border-red-500" : "border-gray-300"
-            } focus:outline-none focus:border-indigo-500`}
+            className={`border-2 rounded-lg p-2 w-full border-gray-600 hover:border-indigo-600 ${
+              errors.creditCard ? "border-red-500" : ""
+            }`}
             maxLength={16}
           />
           {errors.creditCard && (
             <p className="text-red-500 text-sm">{errors.creditCard}</p>
           )}
         </div>
-
-        {/* Expiry Date and CVV */}
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
           <div>
-            <label className="block text-gray-600 font-bold mb-1">
+            <label className="block font-bold text-gray-600 mb-2 ml-1">
               Expiry Date (MM/YY)
             </label>
             <input
@@ -154,9 +156,9 @@ const CheckoutForm = ({ cart }) => {
               name="expiryDate"
               value={formData.expiryDate}
               onChange={handleInputChange}
-              className={`border-2 rounded-lg p-2 w-full text-sm ${
-                errors.expiryDate ? "border-red-500" : "border-gray-300"
-              } focus:outline-none focus:border-indigo-500`}
+              className={`border-2 rounded-lg p-2 w-full border-gray-600 hover:border-indigo-600 ${
+                errors.expiryDate ? "border-red-500" : ""
+              }`}
               maxLength={5}
             />
             {errors.expiryDate && (
@@ -164,24 +166,26 @@ const CheckoutForm = ({ cart }) => {
             )}
           </div>
           <div>
-            <label className="block text-gray-600 font-bold mb-1">CVV</label>
+            <label className="block font-bold text-gray-600 mb-2 ml-1">
+              CVV
+            </label>
             <input
               type="text"
               name="cvv"
               value={formData.cvv}
               onChange={handleInputChange}
-              className={`border-2 rounded-lg p-2 w-full text-sm ${
-                errors.cvv ? "border-red-500" : "border-gray-300"
-              } focus:outline-none focus:border-indigo-500`}
+              className={`border-2 rounded-lg p-2 w-full border-gray-600 hover:border-indigo-600 ${
+                errors.cvv ? "border-red-500" : ""
+              }`}
               maxLength={3}
             />
             {errors.cvv && <p className="text-red-500 text-sm">{errors.cvv}</p>}
           </div>
         </div>
 
-        {/* Contact Field */}
-        <div>
-          <label className="block text-gray-600 font-bold mb-1">
+        {/* Contact Information */}
+        <div className="mb-4">
+          <label className="block font-bold text-gray-600 mb-2 ml-1">
             Contact Number
           </label>
           <input
@@ -189,9 +193,9 @@ const CheckoutForm = ({ cart }) => {
             name="contact"
             value={formData.contact}
             onChange={handleInputChange}
-            className={`border-2 rounded-lg p-2 w-full text-sm ${
-              errors.contact ? "border-red-500" : "border-gray-300"
-            } focus:outline-none focus:border-indigo-500`}
+            className={`border-2 rounded-lg p-2 w-full border-gray-600 hover:border-indigo-600 ${
+              errors.contact ? "border-red-500" : ""
+            }`}
             maxLength={10}
           />
           {errors.contact && (
@@ -200,44 +204,42 @@ const CheckoutForm = ({ cart }) => {
         </div>
 
         {/* Order Summary */}
-        <div>
-          <h4 className="text-lg font-semibold text-center sm:text-left text-gray-700">
-            Order Summary
-          </h4>
-          <ul className="list-disc text-sm pl-5">
-            {cart.map((item) => (
-              <li key={item.id}>
-                {item.title} - {item.quantity} x {"\u20B9"}
-                {item.price.toFixed(2)}
-              </li>
-            ))}
-          </ul>
-          <p className="text-right font-bold">
-            Total: {"\u20B9"}
-            {calculateTotal().toFixed(2)}
-          </p>
-        </div>
+        <h4 className="text-lg text-center text-gray-700 font-semibold mb-2">
+          Order Summary
+        </h4>
+        <ul className="list-disc ml-32 text-gray-600 font-semibold pl-5 mb-4">
+          {cart.map((item) => (
+            <li key={item.id}>
+              {item.title} - {item.quantity} x {"\u20B9"}
+              {item.price.toFixed(2)}
+            </li>
+          ))}
+        </ul>
+        <p className="text-right font-bold text-gray-900">
+          Total: {"\u20B9"}
+          {calculateTotal().toFixed(2)}
+        </p>
 
         {/* Submit Button */}
         <button
           type="submit"
           disabled={isSubmitting}
-          className={`w-full py-2 rounded-lg text-white ${
+          className={` w-full py-2 rounded-lg text-white font-serif ${
             isSubmitting
               ? "bg-gray-400"
-              : "bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300"
+              : "bg-blue-600 hover:bg-blue-700 hover:scale-105 transition-transform duration-300"
           }`}
         >
           {isSubmitting ? "Placing Order..." : "Place Order"}
         </button>
       </form>
 
-      {/* Order Confirmation */}
+      {/* Navigation to Order Confirmation */}
       {!isSubmitting && (
         <div className="mt-4 text-center">
           <NavLink
             to="/order-confirmation"
-            className="text-blue-600 hover:underline"
+            className="text-blue-600 hover:underline hover:text-green-600"
           >
             View Order Confirmation
           </NavLink>
